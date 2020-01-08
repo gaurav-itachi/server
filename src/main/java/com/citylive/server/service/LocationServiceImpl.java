773637b8156ba2cadf76e86f68caac2d642642ec
gauravgsv <gaurav.itachi@gmail.com>
2020-01-09 00:16:16 +0530

    adding rest endpoints and simple brute forece spatial ds for test

--- src/main/java/com/citylive/server/service/LocationServiceImpl.java
+++ src/main/java/com/citylive/server/service/LocationServiceImpl.java
@@ -15,54 +15,53 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-//@RestController
-//@RequestMapping("/locationservice")
-public class LocationServiceImpl{
-//        implements LocationService {
+@RestController
+@RequestMapping("/locationservice")
+public class LocationServiceImpl  implements LocationService {
 
-//
-//    @Autowired
-//    MessagingService messagingService;
-//
-//
-//    SpatialDS<String> spatialDS = new SimpleRangeTree<>();
-//
-//    Map<String,String> db = new HashMap<>();
-//
-//    @RequestMapping("/registerUser/{userid}/{key}")
-//    @Override
-//    public void registerUser(@PathParam("userid") String userId,@PathParam("key") String key) {
-//        db.put(userId,key);
-//    }
-//
-//    @RequestMapping("/updatePos/{userid}/{x}/{y}")
-//    @Override
-//    public void updatePos(String userId, double x, double y) {
-//        spatialDS.updatePos(userId,new Position2D(x,y));
-//    }
-//
-//    @RequestMapping("/notify/{userid}/{x}/{y}/{radius}")
-//    @Override
-//    public ResponseEntity notifyNearbyUsers(@PathParam("userid") String userId,@PathParam("x") double x,@PathParam("y") double y,@PathParam("radius") double radius) {
-//        List<String> usersToNotify = spatialDS.getInRange(new Position2D(x,y),radius);
-//
-//            String res = usersToNotify
-//                    .stream()
-//                    .filter(id->!id.equals(userId))
-//                    .map(id-> {
-//                        try {
-//                            return messagingService.sendNotificationToDevice(db.getOrDefault(id,"random"));
-//                        } catch (FirebaseMessagingException e) {
-//                            //e.printStackTrace();
-//                            System.out.println("error");
-//                        }
-//                        return "error";
-//                    })
-//                    .reduce((a,b)->{
-//                        return a+" "+b;
-//                    })
-//                    .orElse("Error Occured");
-//
-//            return ResponseEntity.ok(res);
-//    }
+
+    @Autowired
+    MessagingService messagingService;
+
+
+    SpatialDS<String> spatialDS = new SimpleRangeTree<>();
+
+    Map<String,String> db = new HashMap<>();
+
+    @RequestMapping("/registerUser/{userid}/{key}")
+    @Override
+    public void registerUser(@PathParam("userid") String userId,@PathParam("key") String key) {
+        db.put(userId,key);
+    }
+
+    @RequestMapping("/updatePos/{userid}/{x}/{y}")
+    @Override
+    public void updatePos(String userId, double x, double y) {
+        spatialDS.updatePos(userId,new Position2D(x,y));
+    }
+
+    @RequestMapping("/notify/{userid}/{x}/{y}/{radius}")
+    @Override
+    public ResponseEntity notifyNearbyUsers(@PathParam("userid") String userId,@PathParam("x") double x,@PathParam("y") double y,@PathParam("radius") double radius) {
+        List<String> usersToNotify = spatialDS.getInRange(new Position2D(x,y),radius);
+
+            String res = usersToNotify
+                    .stream()
+                    .filter(id->!id.equals(userId))
+                    .map(id-> {
+                        try {
+                            return messagingService.sendNotificationToDevice(db.getOrDefault(id,"random"));
+                        } catch (FirebaseMessagingException e) {
+                            //e.printStackTrace();
+                            System.out.println("error");
+                        }
+                        return "error";
+                    })
+                    .reduce((a,b)->{
+                        return a+" "+b;
+                    })
+                    .orElse("Error Occured");
+
+            return ResponseEntity.ok(res);
+    }
 }
